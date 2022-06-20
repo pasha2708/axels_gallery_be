4ff0875eaa59747b535489eb11dd2e9f310a4cab
Mike Gusev <pasha2708@gmail.com>
2022-06-20 20:08:10 +0300

    fix bugs

--- routes/routes.js
+++ routes/routes.js
@@ -3,27 +3,37 @@ const Pictures = require('../models/Schema')
 const router = Router()
 
 router.get('/images', async (req, res) => {
-    const todos = await Pictures.find({})
-    res.send(todos)
+	const data = await Pictures.find({})
+	const images = data.map((item) => (
+		{
+			id: item.id,
+			url: item.url
+		}
+	))
+	res.send(images)
 })
 
 router.get('/images/:id', async (req, res) => {
-    const data = await Pictures.findOne({ id: req.params.id })
-    res.send(data)
+	const data = await Pictures.findOne({ id: req.params.id })
+	if (data) {
+		const { id, fullUrl: url, comments } = data
+		return res.send({ id, url, comments })
+	}
+	return res.status(404).send('not found')
 })
 
 router.post('/images/:id/comments', async (req, res) => {
-    await Pictures.findOneAndUpdate({ id: req.params.id }, { $push: { "comments": req.body } })
-    const picture = await Pictures.findOne({ id: req.params.id })
-    res.send(picture)
+	await Pictures.findOneAndUpdate({ id: req.params.id }, { $push: { "comments": req.body } })
+	const picture = await Pictures.findOne({ id: req.params.id })
+	res.send(picture)
 })
 
 router.delete('/images/:id/comments/:commentId', async (req, res) => {
-    const data = await Pictures.findOne({ id: req.params.id })
-    data.comments = data.comments.filter((e) => e.id != req.params.commentId)
-    await Pictures.findOneAndUpdate({ id: req.params.id }, data)
-    const picture = await Pictures.findOne({ id: req.params.id })
-    res.send(picture)
+	const data = await Pictures.findOne({ id: req.params.id })
+	data.comments = data.comments.filter((e) => e.id != req.params.commentId)
+	await Pictures.findOneAndUpdate({ id: req.params.id }, data)
+	const picture = await Pictures.findOne({ id: req.params.id })
+	res.send(picture)
 })
 
 module.exports = router
