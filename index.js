4073c0556a9426702b6cdb087cfabeb3490215df
Mike Gusev <pasha2708@gmail.com>
2022-06-20 22:18:47 +0300

    layout

--- index.js
+++ index.js
@@ -15,15 +15,15 @@ app.use(cors())
 app.use(todoRoutes)
 
 async function start() {
-	try {
-		await mongoose.connect(
-			'mongodb+srv://pasha:KR7cB9Mr3ehRk7jy@cluster0.e2wkx.mongodb.net/?retryWrites=true&w=majority')
-		app.listen(PORT, () => {
-			console.log(`Server has been started on port ${PORT}`)
-		})
-	} catch (e) {
-		console.log(e)
-	}
+  try {
+    await mongoose.connect(
+      'mongodb+srv://pasha:KR7cB9Mr3ehRk7jy@cluster0.e2wkx.mongodb.net/?retryWrites=true&w=majority')
+    app.listen(PORT, () => {
+      console.log(`Server has been started on port ${PORT}`)
+    })
+  } catch (e) {
+    console.log(e)
+  }
 }
 
 start()
